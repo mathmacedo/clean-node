b292c2900b951faab63fa1952408bccf0b64601d
Matheus Macedo <matheus.macedo1977@gmail.com>
2020-09-21 16:57:47 -0300

    test: create methods to test handle method on signup class

---
+++ src/presententation/controllers/signup.spec.ts
@@ -0,0 +1,31 @@
+import { SignUpController } from './signup'
+
+describe('SingUp Controller', () => {
+  test('Should return 400 if no name is provided ', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no email is provided ', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
